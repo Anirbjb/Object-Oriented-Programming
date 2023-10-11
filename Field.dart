bb2c8228a4238876c524b861be6662197d4950b9
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-11 21:37:45 +0700

    Create Field.dart

---
+++ Field.dart
@@ -0,0 +1,20 @@
+class Person{
+  String name ="Guest";
+  String? address;
+  final String country = "indonesia";
+  
+}
+
+void main(){
+  
+  String name=('guest');
+  String ? address;// data ini menjadi nullable
+  String country=('indonesia');
+  
+  print (name);
+  print(address);
+  print(country);
+  
+  }
+
+
