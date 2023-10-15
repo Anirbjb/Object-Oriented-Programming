552fe7838b2d3ae5fbda0b8f2ce1a0d6eb797c76
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-15 12:23:52 +0700

    Create Class & Object .dart

---
+++ Class & Object .dart
@@ -0,0 +1,15 @@
+class person{ //class
+  
+}
+
+void main(){ //object
+  var person1= person();
+  print(person1);
+  
+  person person2 = person();
+  print(person2);
+  
+  person person3 = person();
+  print(person3);
+  
+}
