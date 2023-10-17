01c935890b7764a75e7443c159af4bf941f50c64
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-17 18:30:35 +0700

    Create Class Object.dart

---
+++ Class Object.dart
@@ -0,0 +1,10 @@
+class Person{}
+
+void main(){ //ini adalah class object parent nya semua method
+   var number= 100;
+  print(number.toString ());
+  
+  var person = Person();
+  print(person.toString());
+  
+}
