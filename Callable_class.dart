eed0097eb2992f24ee8eabe39093b378f060bc2d
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-22 09:42:34 +0700

    Create Callable_class.dart

---
+++ Callable_class.dart
@@ -0,0 +1,17 @@
+  class Sum {
+    int first;
+    int second;
+    
+    
+    Sum(this.first ,this.second);
+    
+    int call() => first  + second;
+  }
+
+void main(){
+  var sum = Sum(30,10);
+  
+  var total = sum();
+  print(total);
+  
+}
